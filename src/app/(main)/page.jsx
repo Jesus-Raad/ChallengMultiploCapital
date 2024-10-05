8ecd596be3f02c85bbc359c80ea0696693e2c29d
JESUSRAAD <raadwebdeveloper@gmail.com>
2024-10-05 15:16:31 +0200

    change start page

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -5,7 +5,7 @@ import Menu from '../../components/Menu'
 const page = () => {
   return (
     <div>
-  INICIO, SELECCIONE UN A DE LAS OPCIONES DEL MENU
+  INICIO, SELECCIONE UN A DE LAS OPCIONES DEL MENU,LA DOCUMENTACION ESTA EN EL README
     </div>
   )
 }
